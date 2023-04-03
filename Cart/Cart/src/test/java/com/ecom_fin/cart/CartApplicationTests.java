df94685ae9f523badda30975e6a825bad8433805
Nitin Kalshetty <nitin939426@gmail.com>
2023-04-03 23:25:50 +0530

    added empty cartModule

---
+++ Cart/Cart/src/test/java/com/ecom_fin/cart/CartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom_fin.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
