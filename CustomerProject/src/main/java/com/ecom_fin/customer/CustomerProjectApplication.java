ae2d56fe5952de2b3cdb2608c15e1494d46ad284
Nitin Kalshetty <nitin939426@gmail.com>
2023-04-08 23:26:56 +0530

    testing

--- CustomerProject/src/main/java/com/ecom_fin/customer/CustomerProjectApplication.java
+++ CustomerProject/src/main/java/com/ecom_fin/customer/CustomerProjectApplication.java
@@ -5,12 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -27,19 +21,6 @@ public class CustomerProjectApplication {
 		return new RestTemplate();
 	}
 
-	@Bean
-	public OAuth2AuthorizedClientManager manager(
-		ClientRegistrationRepository clientRegistrationRepository,
-		OAuth2AuthorizedClientRepository auth2AuthorizedClientRepository
-	){
-
-		OAuth2AuthorizedClientProvider provider = OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build();
-
-		DefaultOAuth2AuthorizedClientManager defaultOAuth2AuthorizedClientManager =  new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository,auth2AuthorizedClientRepository);
-
-		defaultOAuth2AuthorizedClientManager.setAuthorizedClientProvider(provider);
-
-		return defaultOAuth2AuthorizedClientManager;
-	}
+	
 
 }
--- Products/Products/src/main/java/com/ecom_fin/products/controllers/ProductController.java
+++ Products/Products/src/main/java/com/ecom_fin/products/controllers/ProductController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,21 +22,17 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-
-    @PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('SELLER')")
     @PostMapping
     public ResponseEntity<Product> createProductControllerHandler(@RequestBody Product product){
         return new ResponseEntity<Product>(productService.saveProduct(product),HttpStatus.CREATED);
     }
 
-    @PreAuthorize("hasAuthority('SCOPE_internal')")
     @GetMapping
     public ResponseEntity<List<Product>> getAllProductsControllerHandler(){
         return new ResponseEntity<>(productService.getAllProducts(),HttpStatus.ACCEPTED);
     }
 
 
-    @PreAuthorize("hasAuthority('SCOPE_internal')")
     @GetMapping("/{productId}")
     public ResponseEntity<Product> getProductByIdControllerHandler(@PathVariable String productId){
         return new ResponseEntity<>(productService.getProductById(productId),HttpStatus.ACCEPTED);
